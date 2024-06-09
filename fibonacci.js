94046105a545efaf136ac17ec0ff2ae545d5e2e3
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-09 17:57:14 +0300

    Add the recursion fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 
-
-function fibs (number) {
+function fibs(number) {
   if (number <= 0) return []
   else if (number === 1) return [0];
   else {
     let array = [0, 1];
-    let i = 0;
-    let j = 1;
     for (let currentNumber = 2; currentNumber < number; currentNumber++) {
-      array.push (array[i] + array[j])
-      ++i
-      ++j
+      array.push(array[currentNumber - 2] + array[currentNumber - 1])
     }
     return array;
   }
 }
-console.log(`Itteration fibonacci function result : [${fibs(10)}]`)
+
+function fibsRec(number) {
+
+  if (number === 0) return [];
+  if (number === 1) return [0];
+  if (number === 2) return [0, 1];
+
+  let fibs = fibsRec(number - 1); 
+  let nextFib = fibs[fibs.length - 1] + fibs[fibs.length - 2]; 
+  return fibs.concat(nextFib);
+}
+
+console.log(`Itteration fibonacci function result : [${fibs(8)}]`)
+console.log(`Recursion fibonacci function result : [${fibsRec(8)}]`)
